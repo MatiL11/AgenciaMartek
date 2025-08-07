ed6060364498a56a5375c3378ad630738f8bfc4e
matias <matiaslisandro9@gmail.com>
2025-08-07 16:52:48 -0300

    error en el home solucionado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/hero/Hero';
 import OurServices from '../components/ourservices/OurServices';
 import WorkingProcess from '../components/workingProcess/WorkingProcess';
-import SEOAnalysis from '../components/seoAnalysis/SEOAnalysis';
+import SEOAnalysis from '../components/seoAnalysis/SeoAnalysis';
 import ServicesShowcase from '../components/services/ServicesShowcase';
 const Home = () => {
   return (
