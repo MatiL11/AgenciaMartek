9d47eeffd365cca60eca9f13e1442c49b7023d8a
matias <matiaslisandro9@gmail.com>
2025-08-07 16:56:18 -0300

    error seoanalysis fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/hero/Hero';
 import OurServices from '../components/ourservices/OurServices';
 import WorkingProcess from '../components/workingProcess/WorkingProcess';
-import SEOAnalysis from '../components/seoAnalysis/SeoAnalysis';
+import SEOAnalysis from '../components/seoanalysis/SeoAnalysis';
 import ServicesShowcase from '../components/services/ServicesShowcase';
 const Home = () => {
   return (
